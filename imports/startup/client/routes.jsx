f729f74951a2db1942e4dcb78cb0822bea856979
Abdul Hameed <raza2022@gmail.com>
2019-09-12 17:09:51 +0500

    react router config updated and Tracker added for real time data

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { withTracker } from 'meteor/react-meteor-data';
 
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import App from '/imports/ui/components/App/App';
+import Login from '/imports/ui/components/Auth/Login';
+
+
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Routes() {
+    return (
+        <Router>
+            <Route exact path="/" component={App} />
+            <Route exact path="/login" component={Login} />
+        </Router>
+    );
+}
+
+const Root = withTracker(props => {
+    // Do all your reactive data access in this method.
+    // Note that this subscription will get cleaned up when your component is unmounted
+    // const handle = Meteor.subscribe('todoList', props.id);
+
+    return {
+        user: Meteor.user()
+    };
+})(Routes);
+
 
 Meteor.startup(() => {
-    ReactDOM.render(<App />, document.getElementById('render-root'));
+    ReactDOM.render(<Root />, document.getElementById('render-root'));
 });
