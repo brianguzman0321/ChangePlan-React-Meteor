ad6cf6944a97ddb0520cede6680120414b241167
Abdul Hameed <raza2022@gmail.com>
2019-09-18 19:30:49 +0500

    companies and projects tabs separated in control panel

--- imports/ui/components/admin/control-panel/control-panel.js
+++ imports/ui/components/admin/control-panel/control-panel.js
@@ -1,7 +1,107 @@
 import React, {useEffect} from 'react';
-import MaterialTable from 'material-table';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-export default function ProjectsControlPanel() {
+import { Companies } from "/imports/api/companies/companies";
+
+
+import MaterialTable from 'material-table';
+import {createContainer, withTracker} from "meteor/react-meteor-data";
+import {Meteor} from "meteor/meteor";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            <Box p={3}>{children}</Box>
+        </Typography>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: theme.palette.background.paper,
+        // width: 500,
+    },
+}));
+
+function FullWidthTabs(props) {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+
+    function handleChange(event, newValue) {
+        setValue(newValue);
+    }
+
+    function handleChangeIndex(index) {
+        setValue(index);
+    }
+
+    useEffect(() => {
+    });
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    <Tab label="Companies" {...a11yProps(0)} />
+                    <Tab label="Projects" {...a11yProps(1)} />
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    <ProjectsControlPanel {...props}/>
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    Projects
+                </TabPanel>
+            </SwipeableViews>
+        </div>
+    );
+}
+
+//
+function ProjectsControlPanel(props) {
+    const [companies, setCompanies] = React.useState({});
     const [state, setState] = React.useState({
         columns: [
             { title: 'FirstName', field: 'firstName', editable: 'onAdd' },
@@ -17,30 +117,68 @@ export default function ProjectsControlPanel() {
                     activityOwner: 'Activity Owner',
                 },
             },
+            {
+                title: 'Company',
+                field: 'company',
+                lookup: {},
+                // lookup: {
+                //     PTXYQkJd6qwJdRYYD: 'Bakerz'
+                // },
+            },
         ],
         data: [],
-        dataFetched: false
     });
 
-    useEffect((props) => {
-        if(!state.data.length){
-            Meteor.call('users.getAllusers', (err, res) => {
-                console.log(err);
-                if(res){
-                    let data = [...state.data];
-                    data = res.map(user => {
-                        return {
-                            firstName: user.profile.firstName,
-                            lastName: user.profile.lastName,
-                            email: user.emails[0].address,
-                            role: 'manager'
-                        }
 
-                    });
-                    setState({ ...state, data });
-                }
-            })
+    const getUsers = () => {
+        Meteor.call('users.getAllusers', (err, res) => {
+            if(res){
+                let data = [...state.data];
+                data = res.map(user => {
+                    return {
+                        firstName: user.profile.firstName,
+                        lastName: user.profile.lastName,
+                        email: user.emails[0].address,
+                        role: 'manager',
+                        company: 'PTXYQkJd6qwJdRYYD'
+                    }
+
+                });
+                setState({ ...state, data });
+            }
+        })
+    }
+
+    const updateColumns = (companies) => {
+        setCompanies(companies);
+        let columns = [...state.columns];
+        if(!Object.keys(columns[columns.length - 1].lookup).length){
+            columns[columns.length - 1].lookup = companies;
+            console.log("I am just running ", columns[columns.length - 1]);
+            setState({...state, columns});
         }
+
+    };
+
+    useEffect(() => {
+
+        if(!state.data.length){
+            getUsers();
+            console.log("companies", companies);
+            if(!Object.keys(companies).length){
+                if(props.companies && props.companies.length){
+                    let companies1 = props.companies.reduce(function(acc, cur, i) {
+                        acc[cur._id] = cur.name;
+                        return acc;
+                    }, {});
+                    updateColumns(companies1);
+                }
+
+            }
+        }
+
+
+
 
     });
 
@@ -85,3 +223,16 @@ export default function ProjectsControlPanel() {
         />
     );
 }
+
+const ControlPanelPage = withTracker(props => {
+    // Do all your reactive data access in this method.
+    // Note that this subscription will get cleaned up when your component is unmounted
+    // const handle = Meteor.subscribe('todoList', props.id);
+    Meteor.subscribe('companies');
+
+    return {
+        companies: Companies.find({}).fetch()
+    };
+})(FullWidthTabs);
+
+export default ControlPanelPage
