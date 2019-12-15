8b6d5168dec4df5bb678c5d6e64812433d0d02ab
Nikita Zotsik <nikita.exceed@gmail.com>
2019-12-15 16:47:39 +0300

    Fix project duplicating issue

--- imports/ui/components/Projects/Models/DuplicateProject.jsx
+++ imports/ui/components/Projects/Models/DuplicateProject.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Dialog, makeStyles, withStyles} from "@material-ui/core";
+import { Dialog, makeStyles, withStyles } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import {Activities} from "../../../../api/activities/activities";
-import {withTracker} from "meteor/react-meteor-data";
+import { Activities } from "../../../../api/activities/activities";
+import { withTracker } from "meteor/react-meteor-data";
 import MuiDialogTitle from "@material-ui/core/DialogTitle/DialogTitle";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
@@ -59,31 +59,49 @@ function DuplicateProject(props) {
   let newProjectId;
 
   const handleChange = (e) => {
-      setNames(e.target.value)
+    setNames(e.target.value)
   };
 
   const duplicateProject = () => {
-    const newNames = names.split(',');
-    for (let name of newNames) {
-      const params = {
-        project: {
-          name,
-          startingDate: new Date(),
-          endingDate:  new Date(),
-          companyId: company._id,
-        }
-      };
-      Meteor.call('projects.insert', params, (error, result) => {
-        handleClose();
-        newProjectId = result;
-        duplicateActivities();
-      })
+    if (!names.length) {
+      return;
     }
+
+    const { _id, owner, changeManagers = [], peoples = [], stakeHolders = [], managers = [], vision = [], objectives = [], impacts = [], risks = [], peopleCount = 0 } = project;
+
+    const params = {
+      project: {
+        owner,
+        changeManagers,
+        peoples,
+        stakeHolders,
+        managers,
+        vision,
+        objectives,
+        impacts,
+        risks,
+        peopleCount,
+        name: (names || 'Project copy').trim(),
+        startingDate: new Date(),
+        endingDate: new Date(),
+        companyId: company._id,
+      }
+    };
+
+    Meteor.call('projects.insert', params, (error, result) => {
+      if (error) {
+        return;
+      }
+
+      handleClose();
+      newProjectId = result;
+      duplicateActivities();
+    })
   };
 
   const duplicateActivities = () => {
     const projectId = project._id;
-    const activities = Activities.find({projectId}).fetch();
+    const activities = Activities.find({ projectId }).fetch();
     activities.map(newActivity => {
       const paramsActivity = {
         activity: {
@@ -98,7 +116,7 @@ function DuplicateProject(props) {
           time: newActivity.time
         }
       };
-      Meteor.call('activities.insert', paramsActivity, (error, result)=> {
+      Meteor.call('activities.insert', paramsActivity, (error, result) => {
         if (error) {
           console.log('--error-', error.response)
         } else {
@@ -113,16 +131,16 @@ function DuplicateProject(props) {
     <div>
       <Dialog open={open} onClose={() => handleClose()} maxWidth="md" fullWidth={true}>
         <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-          { "Duplicate Project" }
+          {"Duplicate Project"}
         </DialogTitle>
         <DialogContent dividers>
           <Grid container
-                direction="row" >
+            direction="row" >
             <Typography item variant="body1">
               Resulting project name/s
             </Typography>
           </Grid>
-          <TextField onChange={handleChange} id="name" type="text" label="Project name/s"/>
+          <TextField onChange={handleChange} id="name" type="text" label="Project name/s" />
         </DialogContent>
         <DialogActions>
           <Button onClick={() => handleClose()}>
@@ -138,9 +156,9 @@ function DuplicateProject(props) {
 };
 
 export default withTracker(props => {
-  const { project: { _id: projectId} } = props;
+  const { project: { _id: projectId } } = props;
   Meteor.subscribe('compoundActivities', projectId);
   return {
-    activities : Activities.find({projectId}).fetch()
+    activities: Activities.find({ projectId }).fetch()
   };
 })(DuplicateProject);
