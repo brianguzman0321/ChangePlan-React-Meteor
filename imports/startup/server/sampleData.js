ec8cb20f354b7c6327685c31b9e508ed3aa337c5
LiveDream0415 <livedream0415@outlook.com>
2019-12-20 22:28:28 -0500

    Alpha version of gantt timeline with DHTMLX

--- imports/startup/server/sampleData.js
+++ imports/startup/server/sampleData.js
@@ -1,118 +1,143 @@
-import { Companies } from '/imports/api/companies/companies'
-import { Peoples } from '/imports/api/peoples/peoples'
-import { Activities } from '/imports/api/activities/activities'
-import { Projects } from '/imports/api/projects/projects'
-import { Roles } from 'meteor/alanning:roles'
+import { Companies } from "/imports/api/companies/companies";
+import { Peoples } from "/imports/api/peoples/peoples";
+import { Activities } from "/imports/api/activities/activities";
+import { Projects } from "/imports/api/projects/projects";
+import { Roles } from "meteor/alanning:roles";
 
 let companyId, projectId, stackHolderId;
 
 let superAdmin = Meteor.users.findOne({
-    'emails.address': 'raza2022@gmail.com'
+  "emails.address": "raza2022@gmail.com"
 });
 
 let superAdmin1 = Meteor.users.findOne({
-    'emails.address': 'gavin.wedell@changeactivation.com'
+  "emails.address": "gavin.wedell@changeactivation.com"
 });
 
-if(superAdmin1 && superAdmin1._id) {
-    Roles.addUsersToRoles(superAdmin1._id, 'superAdmin', Roles.GLOBAL_GROUP);
+if (superAdmin1 && superAdmin1._id) {
+  Roles.addUsersToRoles(superAdmin1._id, "superAdmin", Roles.GLOBAL_GROUP);
 }
 //
-if(superAdmin && superAdmin._id) {
-    Roles.addUsersToRoles(superAdmin._id, 'superAdmin', Roles.GLOBAL_GROUP);
+if (superAdmin && superAdmin._id) {
+  Roles.addUsersToRoles(superAdmin._id, "superAdmin", Roles.GLOBAL_GROUP);
 
-    if(!Companies.findOne()){
-        console.log("No Companies Exists");
-        companyId = Companies.insert({
-            owner: superAdmin._id,
-            name: "AppBakerZ",
-            peoples: [superAdmin._id]
-        })
-    }
-    // if(!Projects.findOne()){
-    //     console.log("No Projects Exists");
-    //     projectId = Projects.insert({
-    //         owner: superAdmin._id,
-    //         name: 'Change Plan Development',
-    //         companyId: companyId,
-    //         peoples: [superAdmin._id],
-    //         startingDate: new Date(),
-    //         endingDate: new Date()
-    //     })
-    // }
+  if (!Companies.findOne()) {
+    console.log("No Companies Exists");
+    companyId = Companies.insert({
+      owner: superAdmin._id,
+      name: "AppBakerZ",
+      peoples: [superAdmin._id]
+    });
+  }
+  // if(!Projects.findOne()){
+  //     console.log("No Projects Exists");
+  //     projectId = Projects.insert({
+  //         owner: superAdmin._id,
+  //         name: 'Change Plan Development',
+  //         companyId: companyId,
+  //         peoples: [superAdmin._id],
+  //         startingDate: new Date(),
+  //         endingDate: new Date()
+  //     })
+  // }
 
-    // if(!Activities.findOne()){
-    //     console.log("No Activities Exists");
-    //     projectId = Activities.insert({
-    //         owner: superAdmin._id,
-    //         activityOwner: superAdmin._id,
-    //         startingDate: new Date(),
-    //         endingDate: new Date(),
-    //         name: 'email',
-    //         type: 'email',
-    //         description: 'email',
-    //         projectId: projectId,
-    //     })
-    // }
-    // if(!Peoples.findOne()){
-    //     console.log("No StackHolders Exists");
-    //     stackHolderId = Peoples.insert({
-    //         company: companyId,
-    //         businessUnit: 'Development Department',
-    //         name: "Abdul Hameed",
-    //         role: 'Project Manager',
-    //         email: 'raza2022@gmail.com',
-    //         supportLevel: 5,
-    //         influenceLevel: 3
-    //     })
-    // }
+  // if(!Activities.findOne()){
+  //     console.log("No Activities Exists");
+  //     projectId = Activities.insert({
+  //         owner: superAdmin._id,
+  //         activityOwner: superAdmin._id,
+  //         startingDate: new Date(),
+  //         endingDate: new Date(),
+  //         name: 'email',
+  //         type: 'email',
+  //         description: 'email',
+  //         projectId: projectId,
+  //     })
+  // }
+  // if(!Peoples.findOne()){
+  //     console.log("No StackHolders Exists");
+  //     stackHolderId = Peoples.insert({
+  //         company: companyId,
+  //         businessUnit: 'Development Department',
+  //         name: "Abdul Hameed",
+  //         role: 'Project Manager',
+  //         email: 'raza2022@gmail.com',
+  //         supportLevel: 5,
+  //         influenceLevel: 3
+  //     })
+  // }
 }
 
 createSampleUsers = () => {
-    let data = [
-        {
-            email: 'a@a.com',
-            password: 'g1superadmin',
-            profile: {
-                firstName: 'Super',
-                lastName: 'Admin'
-            }
-        },
-        {
-            email: 'b@b.com',
-            password: 'g1admin',
-            profile: {
-                firstName: 'Admin',
-                lastName: 'Admin'
-            }
-        },
-        {
-            email: 'c@c.com',
-            password: 'g1changemanger',
-            profile: {
-                firstName: 'Change',
-                lastName: 'Manager'
-            }
-        },
-        {
-            email: 'd@d.com',
-            password: 'g1manager',
-            profile: {
-                firstName: 'Manager',
-                lastName: 'Manager'
-            }
-        },
-    ];
+  let data = [
+    {
+      email: "a@a.com",
+      password: "g1superadmin",
+      profile: {
+        firstName: "Super",
+        lastName: "Admin"
+      }
+    },
+    {
+      email: "b@b.com",
+      password: "g1admin",
+      profile: {
+        firstName: "Admin",
+        lastName: "Admin"
+      }
+    },
+    {
+      email: "c@c.com",
+      password: "g1changemanger",
+      profile: {
+        firstName: "Change",
+        lastName: "Manager"
+      }
+    },
+    {
+      email: "d@d.com",
+      password: "g1manager",
+      profile: {
+        firstName: "Manager",
+        lastName: "Manager"
+      }
+    }
+  ];
 
-    data.forEach(user => {
-        Accounts.createUser({
-            email : user.email,
-            password : user.password,
-            profile  : user.profile
-        });
-    })
+  data.forEach(user => {
+    Accounts.createUser({
+      email: user.email,
+      password: user.password,
+      profile: user.profile
+    });
+  });
 };
 
 // if(!(superAdmin2 && superAdmin2._id)){
 //     createSampleUsers()
 // }
+let superAdmin2 = Meteor.users.findOne({
+  "emails.address": "mikeman7918@gmail.com"
+});
+if (superAdmin2 && superAdmin2._id) {
+  Roles.addUsersToRoles(superAdmin2._id, "superAdmin", Roles.GLOBAL_GROUP);
+  if (!Companies.findOne()) {
+    console.log("No Companies Exists");
+    companyId = Companies.insert({
+      owner: superAdmin2._id,
+      name: "AppBakerZ",
+      peoples: [superAdmin2._id]
+    });
+  }
+  if (!Projects.findOne()) {
+    console.log("No Projects Exists");
+    projectId = Projects.insert({
+      owner: superAdmin2._id,
+      name: "Change Plan Development",
+      companyId: companyId,
+      peoples: [superAdmin2._id],
+      startingDate: new Date(),
+      endingDate: new Date()
+    });
+  }
+}
--- imports/ui/components/Timeline/Timeline.jsx
+++ imports/ui/components/Timeline/Timeline.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TopNavBar from '/imports/ui/components/App/App'
 import Tabs from '@material-ui/core/Tabs';
+import Button from '@material-ui/core/Button';
 import Tab from '@material-ui/core/Tab';
 import ViewColumnIcon from '@material-ui/icons/ViewColumn';
 import ListIcon from '@material-ui/icons/List';
@@ -9,12 +10,55 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import config from '/imports/utils/config';
 import { withRouter } from 'react-router';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import moment from "moment";
+import {withTracker} from "meteor/react-meteor-data";
+import { withStyles } from '@material-ui/core/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const useStyles = makeStyles({
+//Importing DHTMLX Modules
+//import { gantt } from 'dhtmlx-gantt';
+import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
+
+import { Activities } from '/imports/api/activities/activities'
+import { Projects } from "/imports/api/projects/projects";
+
+import "./Gantt.css";
+const gantt = window.gantt;
+const styles = theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  title: {
+    color: "rgb(70,86,100)",
+    fontWidth: 400,
+    fontSize: "30px !important"
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const useStyles = makeStyles(theme => ({
+  inline: {
+    display:"inline",
+  },
   activityTabs: {
     wrapper: {
       flexDirection:'row',
     },
+    color: "black",
   },
   iconTab: {
     display: 'flex',
@@ -34,23 +78,358 @@ const useStyles = makeStyles({
     lineHeight: 1.5,
     letterSpacing: '-0.015em',
     color: '#465563',
-    marginLeft: 24,
+    marginRight: "20px",
   },
   gridContainer: {
-    overFlow: 'hidden'
+    overFlow: 'hidden',
+    padding: "0 20px",
   },
   topBar: {
     marginTop: 13,
+  },
+  selectExportType: {
+    width: "200px",
+    marginLeft: "20px",
+  },
+  exportDialog: {
+    padding: "20px 30px",
+    width: "800px !important",
+  },
+  flexBox: {
+    display: "flex",
   }
+}));
+const DialogTitle = withStyles(styles)(props => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.title} {...other}>
+      <Typography variant="h2">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
 });
+
+const DialogContent = withStyles(theme => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles(theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+const zoom_tasks = function(dateUnit){
+    switch(dateUnit){
+      case "quarter":
+        gantt.config.scales = [
+          {unit: "day", step: 1, format: "%d, %M"},
+          {
+           unit: "quarter", step: 1, format: function (date) {
+            var dateToStr = gantt.date.date_to_str("%M %Y");
+            var endDate = gantt.date.add(gantt.date.add(date, 3, "month"), -1, "day");
+            return dateToStr(date) + " - " + dateToStr(endDate);
+           }
+         }
+        ];
+        gantt.config.scale_height = 50;
+      break;
+      case "day":
+        gantt.config.scales = [
+          {unit: "day", step: 1, format: "%d %M"}
+        ];
+        gantt.config.scale_height = 27;
+      break;
+      case "week":
+        var weekScaleTemplate = function (date) {
+          var dateToStr = gantt.date.date_to_str("%d %M");
+          var endDate = gantt.date.add(gantt.date.add(date, 1, "week"), -1, "day");
+          return dateToStr(date) + " - " + dateToStr(endDate);
+        };
+        gantt.config.scales = [
+            {unit: "week", step: 1, format: weekScaleTemplate},
+            {unit: "day", step: 1, format: "%D"}
+        ];
+        gantt.config.scale_height = 50;
+      break;
+      case "month":
+        gantt.config.scales = [
+          {unit: "month", step: 1, format: "%F, %Y"},
+          {unit: "day", step: 1, format: "%D, %j"}
+        ];
+        gantt.config.scale_height = 50;
+      break;
+    }
+   // set_scale_units();
+  }
+
+const dateUnit = ["day", "week", "month", "quarter"];
+const colors = {
+  activity: ["rgb(241, 118, 62)","rgb(180, 157, 213)", "rgb(0, 179, 163)"],
+  benefit: "rgb(255, 187, 67)",
+  impact: "rgb(38, 153, 251)",
+  completed: "rgb(0, 179, 163)",
+}
+
+function ChangeManagersNames(project) {
+  if (project.changeManagerDetails) {
+    let changeManagers = project.changeManagerDetails.map(changeManager => {
+      return `${changeManager.profile.firstName} ${changeManager.profile.lastName}`
+    });
+    if (changeManagers.length) {
+      return changeManagers.join(", ")
+    } else {
+      return "-"
+    }
+  }
+}
+
+function Gantt(props) {
+  useEffect(() => {
+    const { tasks, dateUnit} = props;
+    console.log("Events for display", tasks);
+    if(Roles.userIsInRole(Meteor.userId(), 'manager') || Roles.userIsInRole(Meteor.userId(), 'activityOwner') ) {  
+      gantt.config.drag_resize = false;
+      gantt.config.drag_move = false;
+    }
+    gantt.init(this.ganttContainer);
+  //  gantt.config.sort = true;
+    gantt.config.columns = [
+      {
+        name:"eventType", label: "Event type", align:"left"
+      },
+      {
+        name: "stakeholders", label: "Stakeholders"
+      },
+      {
+        name: "owner", label: "Owner"
+      }
+    ]
+    gantt.templates.grid_header_class = function(columnName, column){
+      return "gantt-column-header";
+    };
+
+    gantt.templates.grid_row_class = function(start, end, task){
+      return "grey-background";
+    }
+
+    gantt.templates.task_class = function(start, end, task) {
+      if(task.completed) return "completed-task";
+      else return "";
+    }
+
+    gantt.templates.scale_cell_class = function(date){
+      return "grey-background";
+    }
+
+    gantt.templates.rightside_text = function(start,end, task){
+      if(task.type == gantt.config.types.milestone) {
+        return task.eventType;
+      }
+    }
+    
+    gantt.templates.tooltip_text = function(start,end,task){
+      if(task.description && task.description.length > 20)
+        return task.description.slice(0,20) + "...";
+      else return task.description;
+    };
+
+    gantt.config.tooltip_timeout = 2000;
+
+    console.log("Tasks:", tasks);
+    gantt.parse(tasks);
+  } )
+  return (
+    <div
+      ref={ (input) => { this.ganttContainer = input } }
+      style={ { width: '100%', height: '700px', marginTop:"30px"} }
+    ></div>
+  )
+}
+
 function Timeline(props){
   const classes = useStyles();
-  const [value, setIndex] = React.useState(0);
+  const [viewMode, setViewMode] = useState(0);
+  const [zoomMode, setZoomMode] = useState(0);
+  const [isExporting, setIsExporting] = useState(false);
+  const [exportType, setExportType] = useState(0);
 
-  const handleChange = (event, newValue) => {
-    setIndex(newValue);
+  const handleChangeViewMode = (event, newValue) => {
+    setViewMode(newValue);
   };
+
+  const handleChangeZoomMode = (event, newValue) => {
+    zoom_tasks(dateUnit[newValue]);
+    setZoomMode(newValue);
+  };
+
+  const openExportModal = (event) => {
+    setIsExporting(true);
+  }
+
+  const handleClose = (event) => {
+    setIsExporting(false);
+  }
+
+  const handleExportTypeChange = (event) => {
+    console.log(event.target.value);
+    setExportType(event.target.value);
+  }
+
+  const handleImportClick = (event) => {
+    document.getElementById("import-button").click();
+  }
+
+  const handleImportData = () => {
+    let file = document.getElementById("import-button").files[0];
+    const type = file.name.split('.').pop();
+    let data = {}, len;
+    if(type === "xlsx" || type === "xls") {
+      gantt.importFromExcel({
+        server:"https://export.dhtmlx.com/gantt",
+        data: file,
+        callback: function(project){
+          gantt.clearAll();
+          len = project.length
+          for(i = 0; i < len; i ++)
+            project[i].start_date = moment(project[i].start_date).format("DD-MM-YYYY")
+          console.log("Imported Data:", project);          
+          gantt.parse({data:project});
+        }
+      });
+    }
+    else {
+      gantt.importFromMSProject({
+        data: file,
+        callback: function (project) {
+            console.log("Imported Data:", project);
+            if(project){
+              gantt.clearAll();
+              if (project.config.duration_unit) {
+                  gantt.config.duration_unit = project.config.duration_unit;
+              }                    
+              data = {project}
+            }
+         }
+      });
+    }
+  }
+  const handleDownload = (event) => {
+    console.log(data);
+    switch(exportType) {
+      case 0:
+        gantt.exportToExcel({
+          cellColors: true,
+          columns:[
+            { id:"text",  header:"text", width:30 },
+            { id:"start_date",  header:"start_date", width:30, type: 'date'},
+            { id:"color",  header:"color", width:30 },
+            { id:"description",  header:"description", width:30 },
+            { id:"eventType",  header:"eventType", width:30 },
+            { id:"id",  header:"id", width:30 },
+            { id:"owner",  header:"owner", width:30 },
+            { id:"stakeholders",  header:"stakeholders", width:30 },
+            { id:"type",  header:"type", width:30 }
+          ]
+        });
+        break;
+      case 1:
+        gantt.exportToMSProject();
+        break;
+      case 2:
+        gantt.exportToPDF();
+        break;
+      case 3:
+        gantt.exportToPNG();
+        break;
+    }
+  }
   let menus = config.menus;
+  const [data, setData] = useState({data:[]})
+  useEffect(() => {
+    console.log("Events with all fields", props);
+    let tempData = [];
+    let i, len;
+    const {activities, projects} = props;
+
+    // if(projects[0]) {
+    //   tempData.push({
+    //     id: projects[0]._id,
+    //     eventType: "Project start",
+    //     text: "Project start",
+    //     start_date: moment(projects[0].startingDate).format("DD-MM-YYYY"),
+    //     stakeholders: projects[0].stakeHolders.length,
+    //     owner: projects[0].owner,
+    //     color: "white",
+    //     type: gantt.config.types.milestone,
+    //     description: "Project",
+    //   })
+    // }
+
+    len = activities.length;
+    for(i = 0; i < len; i ++) {
+      let type = activities[i].type;
+      const defaultSteps = ["Awareness", "Preparedness", "Support"];
+      tempData.push({
+        id: activities[i]._id,
+        eventType:activities[i].label || defaultSteps[activities[i].step-1],
+        text: type[0].toUpperCase() +  
+        type.slice(1),
+        start_date: moment(activities[i].dueDate).format("DD-MM-YYYY"), 
+        duration: 1,
+        color: colors.activity[activities[i].step-1],
+        stakeholders: activities[i].stakeHolders.length,
+        owner: activities[i].owner,
+        completed: activities[i].completed,
+        description: activities[i].description,
+      })
+    }
+
+    if(projects[0]) {
+      let owner = ChangeManagersNames(projects[0]);
+      let impacts = projects[0] ? projects[0].impacts:[];
+      len = impacts.length;
+      for(i = 0; i < len; i ++) {
+        tempData.push({
+          id: `impacts #${i}`, 
+          eventType:"Impact",
+          text: `Impact: ${impacts[i].type}`,
+          start_date: moment(impacts[i].expectedDate).format("DD-MM-YYYY"), 
+          duration: 1,
+          color: colors.impact,
+          stakeholders: impacts[i].stakeholders.length,
+          owner: owner,
+          description: impacts[i].description,
+        })
+      }
+
+      let benefits = projects[0] ? projects[0].benefits:[];
+      len = benefits.length;
+      for(i = 0; i < len; i ++) {
+        tempData.push({
+          id: `benefits #${i}`,
+          eventType:"Benefit",
+          text: `Stakeholder benefit`,
+          start_date: moment(benefits[i].dueDate).format("DD-MM-YYYY"), 
+          duration: 1,
+          color: colors.benefit,
+          stakeholders: benefits[i].stakeholders.length,
+          owner: owner,
+          description: benefits[i].description,
+        })
+      }
+    }
+    if(!_.isEqual(data.data, tempData))
+      setData({data:tempData});
+  }, [props])
+
   return (
     <div>
       <TopNavBar menus={menus} {...props} />
@@ -68,27 +447,125 @@ function Timeline(props){
           direction="row"
           justify="space-between"
         >
-          <Grid item xs={3} md={7}>
-            <Typography color="textSecondary" variant="h4" className={classes.topHeading}>
+          <Grid item className={classes.flexBox}>
+            <Typography color="textSecondary" variant="h4" className={classes.topHeading} display="inline">
               Timeline
             </Typography>
-          </Grid>
-          <Grid item xs={6} md={3}>
             <Tabs
-              value={value}
-              onChange={handleChange}
+              value={viewMode}
+              onChange={handleChangeViewMode}
               indicatorColor="primary"
               textColor="primary"
               aria-label="icon tabs example"
+              style={
+                {
+                  background: "white",
+                }
+              }
             >
               <Tab className={classes.activityTab} label={<><div className={classes.iconTab}><ViewColumnIcon/>&nbsp; Gantt</div></>}/>
               <Tab className={classes.activityTab} label={<><div className={classes.iconTab}><ListIcon/>&nbsp; List</div></>}/>
             </Tabs>
           </Grid>
+          <Grid className={classes.flexBox}>
+              <Button 
+                color="primary"
+                onClick={handleImportClick}
+              >
+                Import
+                <input 
+                  type="file"
+                  accept=".xlsx, .xls, .mpp,.xml, text/xml, application/xml, application/vnd.ms-project, application/msproj, application/msproject, application/x-msproject, application/x-ms-project, application/x-dos_ms_project, application/mpp, zz-application/zz-winassoc-mpp" 
+                  onChange={handleImportData}
+                  style={{display:"none"}}
+                  id="import-button"
+                />
+              </Button>
+              <Button 
+                color="primary" 
+                onClick={openExportModal}
+                style={{
+                  marginLeft: "20px"
+                }}
+              >
+                Export
+              </Button>
+            <Tabs
+              value={zoomMode}
+              onChange={handleChangeZoomMode}
+              indicatorColor="primary"
+              textColor="primary"
+              aria-label="icon tabs example"
+              style={{
+                marginLeft: "20px",
+                background: "white",
+              }}
+            >
+              <Tab className={classes.activityTab} label={<><div className={classes.iconTab}>&nbsp; DAY</div></>}/>
+              <Tab className={classes.activityTab} label={<><div className={classes.iconTab}>&nbsp; WEEK</div></>}/>
+              <Tab className={classes.activityTab} label={<><div className={classes.iconTab}>&nbsp; MONTH</div></>}/>
+              <Tab className={classes.activityTab} label={<><div className={classes.iconTab}>&nbsp; QUARTER </div></>}/>
+            </Tabs>
+          </Grid>
         </Grid>
-      </Grid>
+        <Gantt tasks={data}/>
+        <Dialog 
+          onClose={handleClose} 
+          aria-labelledby="customized-dialog-title" 
+          open={isExporting} 
+          className={classes.exportDialog}
+          maxWidth="sm"
+          fullWidth={true}
+        >
+          <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            Export events
+          </DialogTitle>
+          <DialogContent dividers>
+            <Typography gutterBottom>
+              Instruction text!
+            </Typography>
+            <Typography gutterBottom display="inline">
+              Export to 
+            </Typography>
+            <Select
+              label="Select type here"
+              id="demo-simple-select-placeholder-label"
+              value={exportType}
+              onChange={handleExportTypeChange}
+              displayEmpty
+              className={classes.selectEmpty + " " + classes.selectExportType}
+            >
+              <MenuItem value={0}>
+                MS Excel .xls
+              </MenuItem>
+              <MenuItem value={1}>MS Project .mpp</MenuItem>
+              <MenuItem value={2}>PDF document</MenuItem>
+              <MenuItem value={3}>PNG image</MenuItem>
+            </Select>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleDownload} color="primary" >
+              Download
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Grid>         
     </div>
   )
 }
 
-export default withRouter(Timeline)
+const TimelinePage = withTracker(props => {
+  let { match } = props;
+  let { projectId } = match.params;
+  Meteor.subscribe('compoundActivities', projectId);
+  // Meteor.subscribe('myProjects', null, {
+  //     sort: local.sort || {},
+  //     name: local.search
+  // });
+  return {
+      activities : Activities.find().fetch(),
+      projects  : Projects.find(projectId).fetch(),
+  };
+})(withRouter(Timeline));
+
+export default withRouter(TimelinePage)
