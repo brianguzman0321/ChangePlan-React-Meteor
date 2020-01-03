5a4518b8ab7cbc2b7a2915e85b6b348255938bb6
NikitaPlaksijenko <littlepig1012@outlook.com>
2020-01-03 05:50:47 +0100

    Update scale setting

--- imports/ui/components/Timeline/constants.js
+++ imports/ui/components/Timeline/constants.js
@@ -1,4 +1,4 @@
-export const dateUnit = ["day", "week", "month", "quarter"];
+export const scaleTypes = ["day", "week", "month", "quarter"];
 
 export const colors = {
   activity: ["rgb(241, 118, 62)","rgb(180, 157, 213)", "rgb(0, 179, 163)"],
